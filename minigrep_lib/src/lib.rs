36a4ac14339055d149e9ed52fc65194fe755332e
mdgeno <manuelgenomer@gmail.com>
2026-02-02 10:50:54 +0800

    implemented a test function at the test module of the library crate. Further refactoring will still be required.

--- minigrep_lib/src/lib.rs
+++ minigrep_lib/src/lib.rs
@@ -1,18 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-	unimplemented!();
+	vec![]
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn one_result(){
+		let query = "duct";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+		assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+	}
 }
