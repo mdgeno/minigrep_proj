a1593c485cd8c80633f69cc4ac524206ac5973f2
mdgeno <manuelgenomer@gmail.com>
2026-02-05 14:45:48 +0800

    added a test case for a new function feature for case insensitive search use cases

--- minigrep_lib/src/lib.rs
+++ minigrep_lib/src/lib.rs
@@ -11,18 +11,38 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
 	query_list  
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+	vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
 	#[test]
-	fn one_result(){
+	fn case_sensitive(){
 		let query = "duct";
 		let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
 		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
 	}
+
+	#[test]
+	fn case_insensitive(){
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(  
+			vec!["Rust:", "Trust me."], search_case_insensitive(query, contents)
+		);
+	}
 }
+
